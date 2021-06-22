c7bf236bc8134b89c8ef9d925ce72bd92f6e1cb2
ryan-vb <ryanvanbaardwijk@gmail.com>
2021-06-22 20:56:39 +0100

    Update main.py

--- clipboardtools/main.py
+++ clipboardtools/main.py
@@ -1,1 +1,24 @@
+import click
+from tkinter import Tk
+from clipboardtools.tk_windows.function_select import *
+
+plugins = []
+
+
+@click.command(name='start')
+def start():
+    """TODO"""
+    root = Tk()
+
+
+@click.command(name='run')
+@click.Choice(choices=plugins)
+def run_headless():
+    """TODO"""
+    root = Tk()
+    root.withdraw()
+
+if __name__ == '__main__':
+    start()
+
 
