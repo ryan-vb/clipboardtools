c34692b07a0272e0079d38db9a40214966223e1c
ryan-vb <ryanvanbaardwijk@gmail.com>
2021-06-22 20:56:29 +0100

    test scaffold

---
+++ test/test_plugin_import.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class PluginRegistration(unittest.TestCase):
+    def test_should_register_available_plugins(self):
+
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
